17a0da23b28216522e366e8e744242fe629ad34d
BeniAS104 <beniuu104@gmail.com>
2023-10-26 12:12:09 +0200

    aaa
    a

--- homepage.js
+++ homepage.js
@@ -15,4 +15,20 @@ function startTime() {
   }
 
     startTime();
+
+    function getFormattedDate() {
+        const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+      
+        const today = new Date();
+        const dayOfWeek = daysOfWeek[today.getDay()];
+        const month = months[today.getMonth()];
+        const day = today.getDate();
+      
+        return dayOfWeek + ", " + month + " " + day;
+      }
+
+const formattedDate = getFormattedDate();
+
+document.getElementById("formatted-date").textContent = formattedDate;
   
